da469ffe3817906456e6983f64dd67b2ba4ed03c
oigomezz <oigomezz@unal.edu.co>
2024-02-26 18:48:52 -0500

    AdventJS Challenge #05

---
+++ Advent JS/Challenge-05/5.js
@@ -0,0 +1,43 @@
+function cyberReindeer(road, time) {
+  // Code here
+  let response = [road];
+  let count = 1;
+  let newRoad = road.split("");
+  let prevStep = '.';
+
+  while (count < time) {
+    const santaIndex = newRoad.indexOf("S");
+    if (count === 5) {
+      for (let index = 0; index < newRoad.length; index++) {
+        newRoad[index] = newRoad[index] === "|" ? "*" : newRoad[index];
+      }
+    }
+    if (newRoad[santaIndex + 1] !== "|") {
+      newRoad[santaIndex] = prevStep;
+      prevStep = newRoad[santaIndex + 1];
+      newRoad[santaIndex + 1] = "S";
+    }
+    response.push(newRoad.join(""));
+    count++;
+  }
+  return response;
+}
+
+console.log(cyberReindeer("S..|...|..", 10));
+
+// el nivel que piden esos retos cada vez es más alto y estoy empezando a perder cordura
+
+/* -> result:
+[
+  'S..|...|..', // 0 estado inicial
+  '.S.|...|..', // 1 avanza el trineo la carretera
+  '..S|...|..', // 2 avanza el trineo la carretera
+  '..S|...|..', // 3 el trineo para en la barrera
+  '..S|...|..', // 4 el trineo para en la barrera
+  '...S...*..', // 5 se abre la barrera, el trineo avanza
+  '...*S..*..', // 6 avanza el trineo la carretera
+  '...*.S.*..', // 7 avanza el trineo la carretera
+  '...*..S*..', // 8 avanza el trineo la carretera
+  '...*...S..', // 9 avanza por la barrera abierta
+]
+*/
