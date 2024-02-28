327532ee45e2bcbdc2f7d332f462b9fbf07947b3
Oscar Ivan Gomez Zamudio <oigomezz@unal.edu.co>
2024-02-27 20:36:11 -0500

    Advent JS Challenge #09

---
+++ Advent JS/Challenge-09/9.js
@@ -0,0 +1,27 @@
+function adjustLights(lights) {
+  // Code here
+  const result = lights.reduce(
+    (acc, curr, idx) => {
+      const light = idx % 2 ? "🔴" : "🟢";
+      curr !== light ? acc[0]++ : acc[1]++;
+      return acc;
+    },
+    [0, 0]
+  );
+  return Math.min(...result);
+}
+
+console.log(adjustLights(["🟢", "🔴", "🟢", "🟢", "🟢"]));
+// -> 1 (cambias la cuarta luz a 🔴)
+
+console.log(adjustLights(["🔴", "🔴", "🟢", "🔴", "🟢"]));
+// -> 1 (cambia la primera luz a verde)
+
+console.log(adjustLights(["🔴", "🔴", "🟢", "🟢", "🔴"]));
+// -> 2 (cambias la segunda luz a 🟢 y la tercera a 🔴)
+
+console.log(adjustLights(["🟢", "🔴", "🟢", "🔴", "🟢"]));
+// -> 0 (ya están alternadas)
+
+console.log(adjustLights(["🔴", "🔴", "🔴"]));
+// -> 1 (cambias la segunda luz a 🟢)
