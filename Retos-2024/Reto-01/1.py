e8e433ba547118bd05accc3ad98d3f920b8bfe6e
Oscar Ivan Gomez Zamudio <oigomezz@unal.edu.co>
2024-03-05 11:54:34 -0500

    Se ajustan archivos por la extencion Trunk Formatter

--- Retos-2024/Reto-01/1.py
+++ Retos-2024/Reto-01/1.py
@@ -2,7 +2,6 @@ def fizzbuzz(num):
     for number in range(num):
         if number % 3 == 0 and number % 5 == 0:
             print("fizzbuzz")
-        # trunk-ignore(git-diff-check/error)
         elif number % 3 == 0:
             print("fizz")
         elif number % 5 == 0:
