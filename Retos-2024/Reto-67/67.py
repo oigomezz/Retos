df2e517ff3c1e9822cda4e7cab7d7ac1fbbf24bb
Oscar Ivan Gomez Zamudio <oigomezz@unal.edu.co>
2024-03-11 15:33:08 -0500

    Reto #67

---
+++ Retos-2024/Reto-67/67.py
@@ -0,0 +1,29 @@
+def to_octal_and_hex(decimal: int):
+
+    current_decimal = decimal
+
+    # Octal
+    _octal = ""
+    while current_decimal > 0:
+        _octal = str(current_decimal % 8) + _octal
+        current_decimal //= 8
+
+    _octal = 0 if _octal == "" else _octal
+    print(f"{decimal} en octal es {_octal}")
+
+    current_decimal = decimal
+
+    # Hex
+    hex_values = "0123456789ABCDEF"
+    _hex = ""
+    while current_decimal > 0:
+        _hex = hex_values[current_decimal % 16] + _hex
+        current_decimal //= 16
+
+    _hex = 0 if _hex == "" else _hex
+    print(f"{decimal} en hexadecimal es {_hex}")
+
+
+to_octal_and_hex(0)
+to_octal_and_hex(100)
+to_octal_and_hex(1000)
---
+++ Retos-2024/Reto-67/README.md
@@ -0,0 +1,6 @@
+<!-- trunk-ignore-all(prettier) -->
+# Reto #67 OCTAL Y HEXADECIMAL
+
+Crea una función que reciba un número decimal y lo trasforme a Octal y Hexadecimal.
+
+* No está permitido usar funciones propias del lenguaje de programación que realicen esas operaciones directamente.
