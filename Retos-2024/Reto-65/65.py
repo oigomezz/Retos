b0467affe48195f455bcf5aa02dc58e82c5f562c
Oscar Ivan Gomez Zamudio <oigomezz@unal.edu.co>
2024-03-12 12:12:22 -0500

    Se ajustan comentario del Reto #65

--- Retos-2024/Reto-65/65.py
+++ Retos-2024/Reto-65/65.py
@@ -1,6 +1,3 @@
-# Crea una función que sea capaz de detectar si existe un viernes 13
-# en el mes y el año indicados.
-# - La función recibirá el mes y el año y retornará verdadero o falso.
 from datetime import date
 
 def viernes_13(anio:int, mes:int):
