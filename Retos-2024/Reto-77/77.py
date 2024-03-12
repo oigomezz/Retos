f1e3092b0ca9dbbf8f4019b1c6c7450d8f3f192a
Oscar Ivan Gomez Zamudio <oigomezz@unal.edu.co>
2024-03-12 11:52:16 -0500

    Reto #77

---
+++ Retos-2024/Reto-77/77.py
@@ -0,0 +1,26 @@
+import string
+
+
+def caesar_cipher(text: str, decrypt=False, shift=3):
+
+    alphabet = list(string.ascii_lowercase)
+    alphabet.insert(alphabet.index("n") + 1, "ñ")
+
+    caesar_text = ""
+
+    for value in text.lower():
+        if value in alphabet:
+            index = (alphabet.index(value) + (-shift if decrypt else shift)) % len(
+                alphabet
+            )
+            caesar_text += alphabet[index]
+        else:
+            caesar_text += value
+
+    print(caesar_text)
+
+
+caesar_cipher("Mi nombre es oigomezz.")
+caesar_cipher("ol proeuh hv rljrohcc.", True)
+caesar_cipher("Mi nombre es oigomezz.", shift=5)
+caesar_cipher("qn rtqgwj jx tnltqjee.", True, 5)
