a35f84f2d3fe5794d652f5cbd9db0f58e70a9426
Oscar Ivan Gomez Zamudio <oigomezz@unal.edu.co>
2024-03-13 14:41:40 -0500

    Reto #89

---
+++ Retos-2024/Reto-89/89.py
@@ -0,0 +1,22 @@
+def permutations(word: str) -> list:
+
+    if len(word) <= 1:
+        return [word]
+
+    result = []
+
+    for index in range(len(word)):
+
+        current_letter = word[index]
+        rest_word = word[:index] + word[index + 1 :]
+
+        for permutation in permutations(rest_word):
+            result.append(current_letter + permutation)
+
+    return result
+
+
+word = "solas"
+print(f"Permutations of {word}:")
+for index, permutation in enumerate(permutations(word)):
+    print(f"{index + 1}. {permutation}")
