681085e908d7d282ba89addc31485fb0e5de5108
Oscar Ivan Gomez Zamudio <oigomezz@unal.edu.co>
2024-03-13 12:53:09 -0500

    Reto #84

---
+++ Retos-2024/Reto-84/84.py
@@ -0,0 +1,84 @@
+def read_abacus_number(abacus: list) -> str:
+
+    if len(abacus) != 7:
+        raise ValueError("Tamaño total incorrecto")
+
+    number = ""
+
+    for row in abacus:
+        if len(row) == 12 and "---" in row and row.replace("---", "") == "OOOOOOOOO":
+            number += str(len(row.split("---")[0]))
+        else:
+            raise ValueError("Formato de fila incorrecto")
+
+    return "{:,}".format(int(number)).replace(",", ".")
+
+
+print(
+    read_abacus_number(
+        [
+            "O---OOOOOOOO",
+            "OOO---OOOOOO",
+            "---OOOOOOOOO",
+            "OO---OOOOOOO",
+            "OOOOOOO---OO",
+            "OOOOOOOOO---",
+            "---OOOOOOOOO",
+        ]
+    )
+)
+
+
+print(
+    read_abacus_number(
+        [
+            "OOOOOOOOO---",
+            "OOOOOOOOO---",
+            "OOOOOOOOO---",
+            "OOOOOOOOO---",
+            "OOOOOOOOO---",
+            "OOOOOOOOO---",
+            "OOOOOOOOO---",
+        ]
+    )
+)
+
+print(
+    read_abacus_number(
+        [
+            "---OOOOOOOOO",
+            "---OOOOOOOOO",
+            "---OOOOOOOOO",
+            "---OOOOOOOOO",
+            "---OOOOOOOOO",
+            "---OOOOOOOOO",
+            "---OOOOOOOOO",
+        ]
+    )
+)
+print(
+    read_abacus_number(
+        [
+            "O---OOOOOOOO",
+            "OOO---OOOOOO",
+            "---OOOOOOOOO",
+            "OO---OOOOOOO",
+            "OOOOOOO---OO",
+            "OOOOOOOOO---",
+            "---OOOOOOOOO",
+        ]
+    )
+)
+print(
+    read_abacus_number(
+        [
+            "---OOOOOOOOO",
+            "---OOOOOOOOO",
+            "---OOOOOOOOO",
+            "O---OOOOOOOO",
+            "---OOOOOOOOO",
+            "---OOOOOOOOO",
+            "---OOOOOOOOO",
+        ]
+    )
+)
