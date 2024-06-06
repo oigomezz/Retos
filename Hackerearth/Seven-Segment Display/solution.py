d3409ad139fd2c7e1b3f3b496dadeb04e73a44bb
Oscar Ivan Gomez <oigomezz@unal.edu.co>
2024-06-06 11:22:07 -0500

    Seven-Segment Display Python Solution

--- Hackerearth/Seven-Segment Display/solution.py
+++ Hackerearth/Seven-Segment Display/solution.py
@@ -1,5 +1,34 @@
-number = input()
-print(int(number)*2)
+d = {
+    '0': 6,
+    '1': 2,
+    '2': 5,
+    '3': 5,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 3,
+    '8': 7,
+    '9': 6
+}
 
-name = input()
-print('%s' % name)
+t = int(input())
+
+for _ in range(t):
+    n = input().strip()
+    matchsticks = 0
+    for i in n:
+        matchsticks += d[i]
+
+    # Case 1 Max number when the number of Matchstick is even
+    if matchsticks % 2 == 0:
+        x = int(matchsticks/2)
+        number = ''
+        for j in range(x):
+            number = number + '1'
+    else:
+        x = int((matchsticks-3)/2)
+        number = '7'
+        for j in range(x):
+            number = number + '1'
+
+    print(number)
