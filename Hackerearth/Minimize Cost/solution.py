04510b238010ab630297fd2b119c3db6c9b2ef2b
Oscar Ivan Gomez <oigomezz@unal.edu.co>
2024-06-11 09:37:33 -0500

    Minimize Cost Python Solution

--- Hackerearth/Minimize Cost/solution.py
+++ Hackerearth/Minimize Cost/solution.py
@@ -1,10 +1,27 @@
-def Solve(k, arr):
-    # Write code here
-    
+def minimize_cost(k, arr):
+    sum1 = 0
+    counter = 0
+    m = k
 
-n = int(input())
-k = int(input())
+    for i in arr:
+        if (i >= 0):
+            counter += 1
+            sum1 += i
+            m = k
+        else:
+            if (counter == 0):
+                return abs(sum(arr)+i)
+            else:
+                m -= 1
+                if (m < 0):
+                    sum1 += abs(i)
+                else:
+                    sum1 += i
+    return abs(sum1)
+
+
+n, k = map(int, input().split())
 arr = map(int, input().split())
+out_ = minimize_cost(k, arr)
 
-out_ = Solve(k, arr)
 print(out_)
