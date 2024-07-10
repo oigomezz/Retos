356e6f1ceaf60759b12b570388bb619895094ae4
oigomezz <oigomezz@unal.edu.co>
2024-07-10 14:35:49 -0500

    Chocolate Distribution Python Solution

---
+++ Hackerearth/Basics of Implementation/Chocolate Distribution/solution.py
@@ -0,0 +1,23 @@
+T = int(input())
+
+for _ in range(T):
+    n, m = map(int, input().split())
+    a = list(map(int, input().split()))
+
+    mp = {0: -1}
+    prefix = [0] * n
+    sum_val = 0
+    max_val = 0
+
+    for i in range(n):
+        sum_val += a[i]
+        prefix[i] = sum_val
+        if sum_val % m not in mp:
+            mp[sum_val % m] = i
+        else:
+            if sum_val % m == 0:
+                max_val = sum_val
+            else:
+                max_val = max(max_val, sum_val - prefix[mp[sum_val % m]])
+
+    print(max_val // m)
