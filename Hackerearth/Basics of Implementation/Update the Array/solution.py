014d41a778616d2adc10876ce3b85f695141be42
oigomezz <oigomezz@unal.edu.co>
2024-07-15 10:16:39 -0500

    Update the Array Partial Python Solution

--- Hackerearth/Basics of Implementation/Update the Array/solution.py
+++ Hackerearth/Basics of Implementation/Update the Array/solution.py
@@ -2,24 +2,28 @@ def min_updates(n, arr, k):
     if n % 2 != 0:
         return -1
 
-    odd = [x for x in arr if x % 2 != 0]
-    set_odd = set(odd)
-    size_odd = len(set_odd)
-
-    even = [x for x in arr if x % 2 == 0]
-    set_even = set(even)
-    size_even = len(set_even)
-
+    set_arr = set(arr)
+    odd = [x for x in set_arr if x % 2 != 0]
+    even = [x for x in set_arr if x % 2 == 0]
+    
     odd_k = [x for x in range(1, k+1, 1) if x % 2 != 0 and x not in odd]
     even_k = [x for x in range(1, k+1, 1) if x % 2 == 0 and x not in even]
     count = 0
 
-    if size_odd > size_even and (size_odd - size_even) <= len(even_k):
-        count += size_odd - size_even
-    elif size_odd < size_even and (size_even - size_odd) <= len(odd_k):
-        count += size_even - size_odd
-    else:
-        count = -1
+    while len(odd) < n//2 :
+        if len(odd_k) > 0:
+            odd.append(odd_k.pop(0))
+            count += 1
+        else:
+            return -1
+        
+    while len(even) < n//2 :
+        if len(even_k) > 0:
+            even.append(even_k.pop(0))
+            count += 1
+        else:
+            return -1
+
     return count
 
 
