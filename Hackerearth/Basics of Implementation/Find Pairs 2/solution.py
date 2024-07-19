2f8f4f4572aa2a9af4c576299f5b201dd1c4e074
Oscar Ivan Gomez <oigomezz@unal.edu.co>
2024-07-19 10:00:28 -0500

    Find Pairs Python Solution

--- Hackerearth/Basics of Implementation/Find Pairs 2/solution.py
+++ Hackerearth/Basics of Implementation/Find Pairs 2/solution.py
@@ -1,20 +1,45 @@
-N, L, R = [int(x) for x in input().split()]
-li = [int(x) for x in input().split()]
-odd = [x for x in li if x % 2 != 0]
-even = [x for x in li if x % 2 == 0]
-n_odd = len(odd)
-count = 0
-if n_odd == 0:
-    print(count)
-else:
-    odd.sort(reverse=True)
-    even.sort(reverse=True)
-    # print(odd,even)
-    for k in range(n_odd):
-        n_ev = len(even)
-        for i in range(n_ev):
-            if odd[k] + even[i] >= L and odd[k]+even[i] <= R:
-                count += (n_odd - k) * (n_ev - i)
-                even = even[:i]
-                break
-    print(count)
+def binary_search_min(v, prev_val, mn):
+    l, r = 0, len(v) - 1
+    while l <= r:
+        md = (l + r) // 2
+        if v[md] + prev_val > mn:
+            r = md - 1
+        else:
+            l = md + 1
+    return l
+
+
+def binary_search_max(v, prev_val, mx):
+    l, r = 0, len(v) - 1
+    while l <= r:
+        md = (l + r) // 2
+        if v[md] + prev_val > mx:
+            r = md - 1
+        else:
+            l = md + 1
+    return r
+
+
+n, l, r = map(int, input().split())
+v = list(map(int, input().split()))
+v2 = []
+v3 = []
+
+for value in v:
+    if value % 2:
+        v2.append(value)
+    else:
+        v3.append(value)
+
+v2.sort()
+v3.sort()
+nb = 0
+
+for value in v2:
+    r1 = binary_search_max(v3, value, r)
+    l1 = binary_search_min(v3, value, l)
+    if r1 < 0 or l1 > len(v3):
+        continue
+    nb += (r1 - l1 + 1)
+
+print(nb)
