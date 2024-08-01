f93d777450e4a07825db1da93a34321e25e1e514
oigomezz <oigomezz@unal.edu.co>
2024-08-01 11:05:53 -0500

    Missile Bombing C++ Solution

---
+++ Hackerearth/Implementation/Missile Bombing/solution.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+  long long n;
+  long long m;
+  cin >> n >> m;
+  long long array[n + 1][n + 1] = {0};
+
+  while (m--)
+  {
+    long long p, a, b, c, d;
+    cin >> p >> a >> b >> c >> d;
+    long long range1 = 0, range2 = 0;
+    range1 = (c - a);
+    range2 = (d - b);
+    for (long i = a; i <= (a + range1); i++)
+    {
+      for (long j = b; j <= (b + range2); j++)
+      {
+        array[i][j] = (array[i][j] ^ p);
+      }
+    }
+  }
+
+  for (long i = 1; i <= n; i++)
+  {
+    for (long j = 1; j <= n; j++)
+    {
+      cout << array[i][j] << " ";
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
