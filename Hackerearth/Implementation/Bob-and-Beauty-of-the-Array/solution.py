3d8c6552d2158388883aff96089ad815c43b9c9b
oigomezz <oigomezz@unal.edu.co>
2024-08-16 10:14:14 -0500

    Bob and Beauty of the Array

--- Hackerearth/Implementation/Bob-and-Beauty-of-the-Array/solution.py
+++ Hackerearth/Implementation/Bob-and-Beauty-of-the-Array/solution.py
@@ -1,26 +1,15 @@
-def subconjuntos(numeros):
-    return subconjuntos_recursivo([], sorted(numeros))
+import itertools
 
+mod = 1000000007
+n = int(input())
+arr = list(map(int, input().split()))
+subsets = list(
+    map(lambda x: list(itertools.combinations(arr, x)), range(2, len(arr)+1)))
 
-def subconjuntos_recursivo(actual, conjunto):
-    if conjunto:
-        return subconjuntos_recursivo(actual, conjunto[1:]) + subconjuntos_recursivo(actual + [conjunto[0]], conjunto[1:])
-    return [actual]
+result = [item for sublist in subsets for item in sublist]
 
+suma = 0
+for element in result:
+    suma += (max(element)) | min(element)
 
-if __name__ == "__main__":
-
-    n = int(input())
-    arr = list(map(int, input().split()))
-    subconjuntos = subconjuntos(arr)
-    sub2 = []
-
-    for sub in subconjuntos:
-        if len(sub) > 1:
-            sub2.append(sub)
-
-    suma = 0
-    for element in sub2:
-        suma += max(element) | min(element)
-
-    print(suma)
+print(suma % mod)
