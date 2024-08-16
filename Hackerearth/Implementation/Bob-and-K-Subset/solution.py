b43e4d699951c3e5c32a63d67cf348073a5e4380
oigomezz <oigomezz@unal.edu.co>
2024-08-16 10:14:36 -0500

    Bob and K-Subset

--- Hackerearth/Implementation/Bob-and-K-Subset/solution.py
+++ Hackerearth/Implementation/Bob-and-K-Subset/solution.py
@@ -1,28 +1,17 @@
-def subconjuntos(numeros):
-    return subconjuntos_recursivo([], sorted(numeros))
+import itertools
+
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+subsets = list(
+    map(lambda x: list(itertools.combinations(arr, x)), range(1, k+1)))
+result = [item for sublist in subsets for item in sublist]
 
 
-def subconjuntos_recursivo(actual, conjunto):
-    if conjunto:
-        return subconjuntos_recursivo(actual, conjunto[1:]) + subconjuntos_recursivo(actual + [conjunto[0]], conjunto[1:])
-    return [actual]
-
-
-if __name__ == "__main__":
-
-    n, k = map(int, input().split())
-    arr = list(map(int, input().split()))
-    subconjuntos = subconjuntos(arr)
-    sub2 = []
-
-    for sub in subconjuntos:
-        if len(sub) <= k and len(sub) > 0:
-            sub2.append(sub)
-
-    resultado = set()
-    for element in sub2:
-        integer = 0
-        for num in element:
-            integer |= num
-        resultado.add(integer)
-    print(len(resultado))
+resultado = set()
+for element in result:
+    integer = 0
+    for num in element:
+        integer |= num
+    resultado.add(integer)
+print(len(resultado))
