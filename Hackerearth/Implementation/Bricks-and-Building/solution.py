8fd1d846605d590e33651800cb474fa7064fe86d
oigomezz <oigomezz@unal.edu.co>
2024-08-17 09:49:07 -0500

    Bricks and Building

--- Hackerearth/Implementation/Bricks-and-Building/solution.py
+++ Hackerearth/Implementation/Bricks-and-Building/solution.py
@@ -4,11 +4,16 @@ for i in range(n):
     a = int(input())
     buildings[i] = a
 
+queries = {}
 q = int(input())
 for i in range(q):
     count = 0
     k = int(input())
-    for j in range(n):
-        if buildings[j] % k == 0:
-            count += 1
+    if (k in queries):
+        count = queries[k]
+    else :
+        for j in range(n):
+            if buildings[j] % k == 0:
+                count += 1
+        queries[k] = count
     print(count)
