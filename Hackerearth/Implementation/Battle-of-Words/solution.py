e641c6c96ccee3e1e52d50ccf69cc79c7702ae92
oigomezz <oigomezz@unal.edu.co>
2024-08-22 09:00:44 -0500

    Battle Of Words

--- Hackerearth/Implementation/Battle-of-Words/solution.py
+++ Hackerearth/Implementation/Battle-of-Words/solution.py
@@ -1,50 +1,25 @@
-from collections import defaultdict
-
-s1, s2 = "", ""
-
-
-def solve():
-    global s1, s2
-    s1 = input()
-    s2 = input()
-    m1, m2 = defaultdict(int), defaultdict(int)
-
-    for ch in s1:
-        if ch != ' ':
-            m1[ch] += 1
-
-    for ch in s2:
-        if ch != ' ':
-            m2[ch] += 1
-
-    for ch in s2:
-        if ch != ' ':
-            m1[ch] -= 1
-
-    for ch in s1:
-        if ch != ' ':
-            m2[ch] -= 1
-
-    ans, ans2 = "", ""
-
-    for i in m1:
-        if m1[i] > 0:
-            ans += i
-
-    for i in m2:
-        if m2[i] > 0:
-            ans2 += i
-
-    if len(ans) != 0 and len(ans2) == 0:
-        print("You win some.")
-    elif len(ans) != 0 and len(ans2) != 0:
-        print("You draw some.")
-    else:
-        print("You lose some.")
-
-    s1, s2 = "", ""
-
-
 t = int(input())
 for _ in range(t):
-    solve()
+    a = input().strip()
+    b = input().strip()
+    res = 0
+    for i in range(26):
+        c1 = a.count(chr(i + 97))
+        c2 = b.count(chr(i + 97))
+        if c1 != c2:
+            if c1 > c2:
+                if -1 == res:
+                    res = 0
+                    break
+                res = 1
+            elif c1 < c2:
+                if 1 == res:
+                    res = 0
+                    break
+                res = -1
+    if 1 == res:
+        print("You win some.")
+    elif -1 == res:
+        print("You lose some.")
+    elif 0 == res:
+        print("You draw some.")
