52395271661e69b949514b18f7873a47d776433f
oigomezz <oigomezz@unal.edu.co>
2024-10-08 10:58:34 -0500

    Shantam and Richness

---
+++ Hackerearth/Data-Structures/Hash-Tables/Shantam-and-Richness/solution.py
@@ -0,0 +1,20 @@
+t = int(input())
+for _ in range(t):
+    n, m = map(int, input().strip().split())
+    l, r, c, p, q, s = map(int, input().strip().split())
+    donating = [0] * (2 * n)
+    for _ in range(m):
+        donating[l] += c
+        donating[r + 1] -= c
+        li = (l * p + r) % n + 1
+        ri = (r * q + l) % n + 1
+        l = min(ri, li)
+        r = max(ri, li)
+        c = c * s % 1000000 + 1
+    obtain = pos = highest = 0
+    for i in range(n):
+        obtain += donating[i]
+        if obtain > highest:
+            highest = obtain
+            pos = i
+    print(pos, highest)
