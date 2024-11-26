bda2b970f7fd73072a039bd0e8c3df6ad98d2fca
oigomezz <oigomezz@unal.edu.co>
2024-11-26 11:30:40 -0500

    Favourite Singer

--- Hackerearth/Basic-Programming/Input-Output/Favourite-Singer/README.md
+++ Hackerearth/Basic-Programming/Input-Output/Favourite-Singer/README.md
@@ -1,4 +1,4 @@
-# Favourite Singer
+# [Favourite Singer][link]
 
 Bob has a playlist of N songs, each song has a singer associated with it (denoted by an integer)
 
@@ -8,10 +8,11 @@ Count the number of Favourite Singers of Bob
 
 ## Input Format
 
-The first line contains an integer N, denoting the number of songs in Bob's playlist.
-
-The following input contains N integers, ith integer denoting the singer of the ith song.
+- The first line contains an integer N, denoting the number of songs in Bob's playlist.
+- The following input contains N integers, ith integer denoting the singer of the ith song.
 
 ## Output Format
 
 Output a single integer, the number of favourite singers of Bob
+
+[link]: https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/favourite-singer-a18e086a/
--- Hackerearth/Basic-Programming/Input-Output/Favourite-Singer/solution.js
+++ Hackerearth/Basic-Programming/Input-Output/Favourite-Singer/solution.js
@@ -1,18 +1,33 @@
-let myArray = [1, 2, 2, 4, 5, 6, 7, 8, 8, 8];
-let contador = {}; // Creo un objeto para almacenar la cantidad de veces que se repite cada elemento del array.
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+let stdin_input = "";
 
-myArray.forEach(function (x) {
-  // En cada iteración, cuento la cantidad de veces que se repite cada elemento actual del arreglo, y lo almaceno en "contador"
-  contador[x] = (contador[x] || 0) + 1;
+process.stdin.on("data", function (input) {
+  stdin_input += input;
 });
 
-// Hasta aquí ya sabemos la cantidad de veces que se repite cada elemento del arreglo.
-// Entonces, ingresemos a los valores de las claves de "contador" y usamos reduce() para
-// encontrar el número que se repite más veces.
-let masRepetido = Object.keys(contador).reduce(
-  (a, b) => (contador[a] > contador[b] ? a : b),
-  0
-); // aquí retorna el número con mayor cantidad de repeticiones.
+process.stdin.on("end", function () {
+  main(stdin_input);
+});
 
-console.log("Repetido:", contador[masRepetido]); // obtenemos la cantidad de veces que se repitió el número.
-console.log("Numero:", masRepetido);
+function main(input) {
+  const [size, array] = input.split("\n");
+  const dictionary = {};
+
+  const songs = array.split(" ");
+
+  for (const song of songs) {
+    if (dictionary[song]) dictionary[song] += 1;
+    else dictionary[song] = 1;
+  }
+
+  let counter = 0;
+  let key = 0;
+
+  for (const x in dictionary)
+    if (counter <= dictionary[x]) counter = dictionary[x];
+
+  for (const x in dictionary) if (counter === dictionary[x]) key += 1;
+
+  process.stdout.write(key);
+}
