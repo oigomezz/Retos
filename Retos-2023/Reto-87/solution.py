ffa8e2cb3c2d06b03e02c71a214be3439b39b146
oigomezz <oigomezz@unal.edu.co>
2024-12-05 09:59:16 -0500

    Reto #87 EL TXT

--- Retos-2023/Reto-87/solution.py
+++ Retos-2023/Reto-87/solution.py
@@ -1,12 +1,10 @@
 import os
 
 file_name = "text.txt"
-
 if os.path.exists(file_name):
-
     print("El fichero ya existe. ¿Qué quieres hacer?")
-    text = input('Pulsa "enter" para continuar o escribe "delete" para eliminar: ')
-
+    text = input(
+        'Pulsa "enter" para continuar o escribe "delete" para eliminar: ')
     if text == "delete":
         os.remove(file_name)
     else:
@@ -14,15 +12,10 @@ if os.path.exists(file_name):
         print(file.read())
 
 file = open(file_name, "a")
-
 while True:
-
     text = input('Escribe el texto a añadir o "exit" para salir: ')
-
     if text == "exit":
         break
-
     file.write(text + "\n")
     file.flush()
-
 file.close()
