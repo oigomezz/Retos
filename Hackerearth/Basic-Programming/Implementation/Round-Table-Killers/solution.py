14e60ea955af9d4e5566cbd6d9ff418996f5dbb4
oigomezz <oigomezz@unal.edu.co>
2024-12-24 15:00:00 -0500

    Round Table Killers

--- Hackerearth/Basic-Programming/Implementation/Round-Table-Killers/solution.py
+++ Hackerearth/Basic-Programming/Implementation/Round-Table-Killers/solution.py
@@ -1,17 +1,13 @@
-n, k, x = map(int, input().split())
-l = [_ for _ in range(1, n+1)]
-
-while (len(l) != 1):
-    q = x % k
-    i = l.index(x)
-    a = l[:i]
-    del l[:i]
-    for j in a:
-        l.append(j)
-
-    i = l.index(x)
-    del l[i+1:i+1+q]
-    if len(l) > 1:
-        x = l[i+1]
-
-print(l[0])
+n, k, x = map(int, input().strip().split())
+persons = list(range(1, n + 1))
+while len(persons) > 1:
+    i = persons.index(x)
+    pre = persons[:i]
+    del persons[:i]
+    persons.extend(pre)
+    i = persons.index(x)
+    i += 1
+    del persons[i:i + x % k]
+    if len(persons) > 1:
+        x = persons[i]
+print(persons[0])
