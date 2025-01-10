6745f928388d8c8a9c7cb6f36da45c85cdf539f3
oigomezz <oigomezz@unal.edu.co>
2025-01-10 09:20:30 -0500

    Three arrays

--- Hackerearth/Basic-Programming/Recursion/Three-arrays/solution.py
+++ Hackerearth/Basic-Programming/Recursion/Three-arrays/solution.py
@@ -1,37 +1,18 @@
+def solve(ans, l, r, m):
+    count = 0
+    for i in range(len(ans)):
+        if ans[i] == l*m or ans[i] == r*m:
+            count += 1
+    return count
+
+
 n, m, k = map(int, input().split())
-a = []
-b = []
-c = []
-l = []
-r = []
-
+ans = []
 for i in range(n):
-    ai, bi, ci = map(int, input().split())
-    a.append(ai)
-    b.append(bi)
-    c.append(ci)
+    a, b, c = map(int, input().split())
+    temp = m*a+m*b-m*c
+    ans.append(temp)
 
-for i in range(3):
-    li, ri = map(int, input().split())
-    l.append(li)
-    r.append(ri)
-
-l1 = l[0]
-l2 = l[1]
-l3 = l[2]
-r1 = min(l[0] + m, r[0]) + 1
-r2 = min(l[1] + m, r[1]) + 1
-r3 = min(l[2] + m, r[2]) + 1
-
-for i in range(l1, r1):
-    for j in range(l2, r2):
-        for k in range(l3, r3):
-            cnt = 0
-            for l in range(n):
-                if ((i * a[l] + j * b[l] - k * c[l]) % m == 0):
-                    cnt += 1
-            if cnt == k:
-                print(i, j, k)
-                exit(0)
-
-print("-1")
+for i in range(m):
+    l, r = map(int, input().split())
+    print(solve(ans, l, r, m), end=' ')
