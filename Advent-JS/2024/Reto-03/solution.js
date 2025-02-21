a21214da57890bdc5af62c70c91c8a89959c8c75
oigomezz <oigomezz@unal.edu.co>
2025-02-21 09:36:25 -0500

    🏗️ Organizando el inventario

--- Advent-JS/2024/Reto-03/solution.js
+++ Advent-JS/2024/Reto-03/solution.js
@@ -1,8 +1,24 @@
+/**
+ * @typedef {Object} InventoryItem
+ * @property {string} name - The name of the inventory item.
+ * @property {number} quantity - The quantity of the inventory item.
+ * @property {string} category - The category of the inventory item.
+ */
+
+/**
+ * @typedef {Object.<string, Object.<string, number>>} OrganizedInventory
+ */
+
+/**
+ * @param {InventoryItem[]} inventory
+ * @returns {OrganizedInventory}
+ */
 function organizeInventory(inventory) {
-  return inventory.reduce((r, toy) => {
-    r[toy.category] ??= {};
-    r[toy.category][toy.name] ??= 0;
-    r[toy.category][toy.name] += toy.quantity;
-    return r;
+  return inventory.reduce((acc, item) => {
+    const { category, name, quantity } = item;
+    acc[category] ||= {};
+    acc[category][name] = (acc[category][name] ?? 0) + quantity;
+
+    return acc;
   }, {});
 }
