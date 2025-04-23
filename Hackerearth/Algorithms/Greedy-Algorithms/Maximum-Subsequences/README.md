ac4901fdc5aa3922ede793908d4eefab5802c95b
oigomezz <oigomezz@unal.edu.co>
2025-04-23 09:21:06 -0500

    ✨ Maximum subsequences

---
+++ Hackerearth/Algorithms/Greedy-Algorithms/Maximum-Subsequences/README.md
@@ -0,0 +1,18 @@
+# [Maximum subsequences][link]
+
+Consider a string s of length n that consists of lowercase Latin alphabetic characters. You are given an array A of size 26 showing the value for each alphabet. You must output the subsequence of size k whose sum of values is maximum.
+
+If there are multiple subsequences available, then print the lexicographically smallest sequence.
+
+String p is lexicographically smaller than string q if p is a prefix of q and is not equal to q or there exists i, such that pi < qi and for all j < i it is satisfied that pj = qj.
+
+## Input format
+
+- The first line contains an integer t representing the number of test cases.
+- For each test case, you are given two integers n and k, a string s, and an array A of length 26.
+
+## Output format
+
+Print a string for each test case.
+
+[link]: https://www.hackerearth.com/practice/algorithms/greedy/basics-of-greedy-algorithms/practice-problems/algorithm/maximal-subsequence-c9f19b0a/
---
+++ Hackerearth/Algorithms/Greedy-Algorithms/Maximum-Subsequences/solution.py
@@ -0,0 +1,55 @@
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().strip().split())
+    s = input()
+    a = list(map(int, input().strip().split()))
+    arr = [(chr(97 + i), x) for i, x in enumerate(a)]
+    arr.sort(key=lambda x: -x[1])
+    included = set()
+    chars = 0
+    index = 0
+    last_char = ''
+    last_char_included = 0
+    while True:
+        if arr[index][0] in s:
+            if s.count(arr[index][0]) >= k - chars:
+                last_char = arr[index][0]
+                last_char_included = k - chars
+            else:
+                included.add(arr[index][0])
+            chars += min(s.count(arr[index][0]), k - chars)
+        index += 1
+        if chars >= k:
+            break
+    if k == 1:
+        print(last_char)
+    else:
+        res = []
+        last_chars = 0
+        last_char_remained = s.count(last_char)
+        for char in s:
+            if char in included:
+                if last_chars > 0:
+                    if char > last_char:
+                        min_last_chars = min(last_chars, last_char_included)
+                        res.extend(last_char * min_last_chars)
+                        last_char_included -= min_last_chars
+                    else:
+                        if last_char_included > last_char_remained - last_chars:
+                            new_last_char_included = last_char_remained - last_chars
+                            res.extend(
+                                last_char * (last_char_included - new_last_char_included))
+                            last_char_included = new_last_char_included
+                last_char_remained -= last_chars
+                last_chars = 0
+                res.append(char)
+            elif char == last_char:
+                last_chars += 1
+        if last_chars > 0:
+            if s[-1] > last_char:
+                res.extend(last_char * min(last_chars, last_char_included))
+            else:
+                if last_char_included > last_char_remained - last_chars:
+                    res.extend(last_char * (last_char_included -
+                               last_char_remained + last_chars))
+        print(*res, sep='')
