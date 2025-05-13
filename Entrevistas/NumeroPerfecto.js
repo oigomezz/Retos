6ceb5694e5c02ca0620ce7c831d5f1ad29a58026
oigomezz <oigomezz@unal.edu.co>
2025-05-13 12:01:19 -0500

    ♻️Se re-organiza los ejercicios practicos de entrevistas

--- Entrevistas/NumeroPerfecto.js
+++
@@ -1,6 +0,0 @@
-function perfectNumber(num) {
-  if (num <= 1) return false;
-  let divisors = 0;
-  for (let i = 1; i < num; i++) if (num % i === 0) divisors += i;
-  return divisors === num;
-}
--- Entrevistas/NumeroPrimo.js
+++
@@ -1,5 +0,0 @@
-function primeNum(num) {
-  if (num === 1) return false;
-  for (let i = 2; i < num / 2; i++) if (num % i === 0) return false;
-  return true;
-}
