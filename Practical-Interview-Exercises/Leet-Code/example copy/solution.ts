4afa4e73b485bf9a57e31d37be2730ce5f42d878
oigomezz <oigomezz@unal.edu.co>
2025-05-19 11:59:03 -0500

    ✨ Same Tree

---
+++ Practical-Interview-Exercises/Leet-Code/example copy/solution.ts
@@ -0,0 +1,20 @@
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val ?? 0;
+    this.left = left ?? null;
+    this.right = right ?? null;
+  }
+}
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+  if (p === null && q === null) return true;
+  if (p === null || q === null) return false;
+  return (
+    p.val === q.val &&
+    isSameTree(p.left, q.left) &&
+    isSameTree(p.right, q.right)
+  );
+}
