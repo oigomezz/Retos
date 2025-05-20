401d93be2dea6d576c15e0fe3a9b4a23f88bf58e
oigomezz <oigomezz@unal.edu.co>
2025-05-20 14:31:19 -0500

    ✨ Maximum Depth of Binary Tree

---
+++ Practical-Interview-Exercises/Leet-Code/Maximum-Depth-of-Binary-Tree/solution.ts
@@ -0,0 +1,6 @@
+function maxDepth(root: TreeNode | null): number {
+  if (root === null) return 0;
+  const leftDepth = maxDepth(root.left);
+  const rightDepth = maxDepth(root.right);
+  return Math.max(leftDepth, rightDepth) + 1;
+}
