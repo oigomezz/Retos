2b5de5d7718aec1af37516a4e8be3bc6de145f05
oigomezz <oigomezz@unal.edu.co>
2025-05-20 14:55:46 -0500

    ✨ Balanced Binary Tree

---
+++ Practical-Interview-Exercises/Leet-Code/Balanced-Binary-Tree/solution.ts
@@ -0,0 +1,18 @@
+import { TreeNode } from "../../tree-node";
+
+function isBalanced(root: TreeNode | null): boolean {
+  function check(node: TreeNode | null): number {
+    if (node === null) return 0;
+
+    const left = check(node.left);
+    if (left === -1) return -1;
+
+    const right = check(node.right);
+    if (right === -1) return -1;
+
+    if (Math.abs(left - right) > 1) return -1;
+
+    return Math.max(left, right) + 1;
+  }
+  return check(root) !== -1;
+}
