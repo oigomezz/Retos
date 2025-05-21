c10d7c9d12625eaabb00142e9cac007f51cf8974
oigomezz <oigomezz@unal.edu.co>
2025-05-21 14:26:18 -0500

    ✨ Path Sum

---
+++ Practical-Interview-Exercises/Leet-Code/Path-Sum/solution.ts
@@ -0,0 +1,22 @@
+import { TreeNode } from "../../tree-node";
+
+function hasPathSum(root: TreeNode | null, targetSum: number): boolean {
+  if (!root) return false;
+
+  const stack = [[root, root.val]];
+  while (stack.length > 0) {
+    const item = stack.pop();
+    if (!item) break;
+
+    const [node, currentSum] = item as [TreeNode, number];
+    const left = node.left;
+    const right = node.right;
+
+    if (!left && !right && currentSum === targetSum) return true;
+
+    if (left) stack.push([left, currentSum + left.val]);
+    if (right) stack.push([right, currentSum + right.val]);
+  }
+
+  return false;
+}
