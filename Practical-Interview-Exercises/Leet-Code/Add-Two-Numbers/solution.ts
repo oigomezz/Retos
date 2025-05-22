22c93cf22e2f810cd29d9c4412aea53c832c172b
oigomezz <oigomezz@unal.edu.co>
2025-05-22 16:09:17 -0500

    ✨ Add Two Numbers

---
+++ Practical-Interview-Exercises/Leet-Code/Add-Two-Numbers/solution.ts
@@ -0,0 +1,26 @@
+import { ListNode } from "../../list-node";
+
+function addTwoNumbers(
+  l1: ListNode | null,
+  l2: ListNode | null
+): ListNode | null {
+  const dummyHead = new ListNode(0);
+  let p = l1,
+    q = l2,
+    current = dummyHead;
+  let carry = 0;
+
+  while (p || q || carry) {
+    const x = p ? p.val : 0;
+    const y = q ? q.val : 0;
+    const sum = x + y + carry;
+    carry = Math.floor(sum / 10);
+    current.next = new ListNode(sum % 10);
+    current = current.next;
+
+    if (p) p = p.next;
+    if (q) q = q.next;
+  }
+
+  return dummyHead.next;
+}
