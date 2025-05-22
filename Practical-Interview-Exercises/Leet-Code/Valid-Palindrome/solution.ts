f56fe4d6096c49dfa28d4e906175b16a2fb34cc4
oigomezz <oigomezz@unal.edu.co>
2025-05-22 15:02:44 -0500

    ✨ Valid Palindrome

---
+++ Practical-Interview-Exercises/Leet-Code/Valid-Palindrome/solution.ts
@@ -0,0 +1,12 @@
+function isPalindrome(s: string): boolean {
+  const cleaned = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  let left = 0;
+  let right = cleaned.length - 1;
+
+  while (left < right) {
+    if (cleaned[left] !== cleaned[right]) return false;
+    left++;
+    right--;
+  }
+  return true;
+}
