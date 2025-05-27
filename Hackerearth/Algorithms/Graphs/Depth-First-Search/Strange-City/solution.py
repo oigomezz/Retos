ee1f59a46dee6473528931e8ee50249fc6ba26dd
oigomezz <oigomezz@unal.edu.co>
2025-05-27 09:58:41 -0500

    ♻️ Refactor Strange City

--- Hackerearth/Algorithms/Graphs/Depth-First-Search/Strange-City/solution.py
+++ Hackerearth/Algorithms/Graphs/Depth-First-Search/Strange-City/solution.py
@@ -1,71 +1,116 @@
-cost = [0] * 100005
-adj = [[] for _ in range(100005)]
-tree = [[] for _ in range(4000020)]
-start = [0] * 100005
-end_ = [0] * 100005
-time_ = [0] * 100005
-vis = [0] * 100005
-k = 0
+class seg_tree:
+    def __init__(self, a):
+        n = len(a)
+        self.n = n
+        self.value = [None] * 2 * n
+        for i in range(n):
+            self.value[i + n] = [a[i]]
+        for i in range(2 * n - 1, 1, -2):
+            self.value[i >> 1] = self.merge_sort(
+                self.value[i], self.value[i ^ 1])
+
+    def merge_sort(self, a, b):
+        res = []
+        i = 0
+        j = 0
+        while True:
+            if i >= len(a):
+                for k in range(j, len(b)):
+                    res.append(b[k])
+                break
+            if j >= len(b):
+                for k in range(i, len(a)):
+                    res.append(a[k])
+                break
+            if a[i] < b[j]:
+                res.append(a[i])
+                i += 1
+            elif a[i] > b[j]:
+                res.append(b[j])
+                j += 1
+            else:
+                res.append(a[i])
+                i += 1
+        return res
+
+    def binary_search_floor(self, x, a):
+        l = 0
+        r = len(a) - 1
+        while l <= r:
+            i = (l + r) // 2
+            if a[i] <= x:
+                l = i + 1
+            else:
+                r = i - 1
+        return r
+
+    def query(self, l, r, k):
+        l += self.n
+        r += self.n
+        res = 0
+        while l <= r:
+            if l & 1 != 0:
+                i = self.binary_search_floor(k, self.value[l])
+                res += i + 1
+            if r & 1 == 0:
+                i = self.binary_search_floor(k, self.value[r])
+                res += i + 1
+            l = (l + 1) >> 1
+            r = (r - 1) >> 1
+        return res
 
 
-def dfs(source):
-    global k
-    vis[source] = 1
-    k += 1
-    time_[k] = source
-    start[source] = k
-    for neighbor in adj[source]:
-        if not vis[neighbor]:
-            dfs(neighbor)
-    end_[source] = k
-
-
-def build(node, left, right):
-    if left > right:
-        return
-    if left == right:
-        tree[node].append(cost[time_[left]])
-        return
-    mid = left + (right - left) // 2
-    build(2 * node, left, mid)
-    build(2 * node + 1, mid + 1, right)
-    tree[node] = sorted(tree[2 * node] + tree[2 * node + 1])
-
-
-def query(node, left, right, k, start_range, end_range):
-    if start_range > right or end_range < left:
-        return 0
-    if start_range >= left and end_range <= right:
-        if len(tree[node]) == 0:
-            return 0
-        pos = next((i for i, value in enumerate(
-            tree[node]) if value >= k), len(tree[node]))
-        return len(tree[node]) - pos
-    mid = (end_range + start_range) // 2
-    t1 = query(2 * node, left, right, k, start_range, mid)
-    t2 = query(2 * node + 1, left, right, k, mid + 1, end_range)
-    return t1 + t2
+def dfs(adj, costs):
+    n = len(adj)
+    visited = [False] * n
+    visited[0] = True
+    pointer = [0] * n
+    start = [0] * n
+    end = [0] * n
+    costs_linear = [costs[0]]
+    stack = [0]
+    i = 0
+    while stack:
+        x = stack[-1]
+        any_more = False
+        for j in range(pointer[x], len(adj[x])):
+            y = adj[x][j]
+            pointer[x] += 1
+            if not visited[y]:
+                i += 1
+                start[y] = i
+                stack.append(y)
+                costs_linear.append(costs[y])
+                any_more = True
+                visited[y] = True
+                break
+        if not any_more:
+            stack.pop()
+            end[x] = i
+    return start, end, costs_linear
 
 
 if __name__ == "__main__":
-    n, q, d = map(int, input().split())
-    arr = list(map(int, input().split()))
-    for i in range(1, n + 1):
-        cost[i] = arr[i - 1]
-        vis[i] = 0
-    for i in range(1, n):
-        u, v = map(int, input().split())
-        adj[u].append(v)
-        adj[v].append(u)
-
-    dfs(d)
-    build(1, 1, n)
-    last = 0
+    n, q, d = list(map(int, input().strip().split()))
+    d -= 1
+    costs = list(map(int, input().strip().split()))
+    adj = [[] for _ in range(n)]
+    for _ in range(n - 1):
+        x, y = list(map(int, input().strip().split()))
+        adj[x - 1].append(y - 1)
+        adj[y - 1].append(x - 1)
+    start, end, costs_linear = dfs(adj, costs)
+    st = seg_tree(costs_linear)
+    ans = 0
     for _ in range(q):
-        line = list(map(int, input().split()))
-        r = line[0] ^ last
-        x = line[1] ^ last
-        y = line[2] ^ last
-        last = query(1, start[r] + 1, end_[r], x - cost[r] - cost[d], 1, n) - \
-            query(1, start[r] + 1, end_[r], y - cost[r] - cost[d] + 1, 1, n)
-        print(last)
+        k1, k2, k3 = list(map(int, input().strip().split()))
+        r, x, y = k1 ^ ans, k2 ^ ans, k3 ^ ans
+        r -= 1
+        if y - costs[r] - costs[d] <= 0:
+            ans = 0
+        elif x - costs[r] - costs[d] <= 1:
+            ans = st.query(start[r] + 1, end[r], y - costs[r] - costs[d])
+        else:
+            ans = st.query(start[r] + 1, end[r], y - costs[r] - costs[d]) - \
+                st.query(start[r] + 1, end[r], x - costs[r] - costs[d] - 1)
+        print(ans)
