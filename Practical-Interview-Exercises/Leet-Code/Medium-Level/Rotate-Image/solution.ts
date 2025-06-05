af5c58ad4452506f87fc8cf23da17682eba1c239
oigomezz <oigomezz@unal.edu.co>
2025-06-05 15:39:30 -0500

    ✨ Rotate Image

---
+++ Practical-Interview-Exercises/Leet-Code/Medium-Level/Rotate-Image/solution.ts
@@ -0,0 +1,13 @@
+function rotate(matrix: number[][]): void {
+  const n = matrix.length;
+  // Transpose the matrix
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    }
+  }
+  // Reverse each row
+  for (let i = 0; i < n; i++) {
+    matrix[i].reverse();
+  }
+}
