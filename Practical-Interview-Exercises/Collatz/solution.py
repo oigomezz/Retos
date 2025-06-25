42589f46f2dd4273997a25c26f50032300e3161b
oigomezz <oigomezz@unal.edu.co>
2025-06-25 10:10:30 -0500

    ✨ Collatz

---
+++ Practical-Interview-Exercises/Collatz/solution.py
@@ -0,0 +1,34 @@
+data = {
+    1: [4, 2, 1]
+}
+
+
+def collatz(n):
+    if n not in data:
+        path = []
+        next_n = n 
+        while next_n > 1:
+            next_n = next_n//2 if next_n % 2 == 0 else 3 * next_n + 1
+            path.append(next_n)
+        data[n] = path
+        return path
+    else:
+        return data[n]
+
+
+if __name__ == "__main__":
+    with open('collatz.txt', 'r') as archivo:
+        separador = ':'
+        for linea in archivo:
+            key, value = linea.split(separador)
+            key = int(key.strip())
+            value = value.replace("[", "").replace("]", "").strip()
+            data[key] = [int(x) for x in value.split(",")]
+
+    for i in range(2, 30):
+        collatz(i)
+
+    ordenados = dict(sorted(data.items(), key=lambda item: int(item[0])))
+
+    for key, value in ordenados.items():
+        print(f"{key}: {value}")
