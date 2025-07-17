1b96fe4e471c3e75846cacc3b0b32df313675ca9
oigomezz <oigomezz@unal.edu.co>
2025-07-17 11:29:16 -0500

    ✨ Validate Binary Search Tree

---
+++ Practical-Interview-Exercises/Leet-Code/Medium-Level/Validate-Binary-Search-Tree/solution.ts
@@ -0,0 +1,19 @@
+import { TreeNode } from "../../../tree-node";
+
+function isValidBST(root: TreeNode | null): boolean {
+  function valid(
+    node: TreeNode | null,
+    minimum: number,
+    maximum: number
+  ): boolean {
+    if (!node) return true;
+    if (!(node.val > minimum && node.val < maximum)) return false;
+
+    return (
+      valid(node.left, minimum, node.val) &&
+      valid(node.right, node.val, maximum)
+    );
+  }
+
+  return valid(root, -Infinity, Infinity);
+}
