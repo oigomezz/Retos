4cf6e31f7e15561fe2ea0bb8f88ed862904fec7b
oigomezz <oigomezz@unal.edu.co>
2025-07-17 11:34:57 -0500

    ✨ Recover Binary Search Tree

---
+++ Practical-Interview-Exercises/Leet-Code/Medium-Level/Recover-Binary-Search-Tree/solution.ts
@@ -0,0 +1,42 @@
+import { TreeNode } from "../../../tree-node";
+
+function recoverTree(root: TreeNode | null): void {
+  let first: TreeNode | null = null,
+    second: TreeNode | null = null,
+    prev: TreeNode | null = null;
+  let curr: TreeNode | null = root,
+    pred: TreeNode | null = null;
+
+  while (curr) {
+    if (curr.left) {
+      pred = curr.left;
+      while (pred.right && pred.right !== curr) pred = pred.right;
+
+      if (!pred.right) {
+        pred.right = curr;
+        curr = curr.left;
+      } else {
+        if (prev && prev.val > curr.val) {
+          if (!first) first = prev;
+          second = curr;
+        }
+        pred.right = null;
+        prev = curr;
+        curr = curr.right;
+      }
+    } else {
+      if (prev && prev.val > curr.val) {
+        if (!first) first = prev;
+        second = curr;
+      }
+      prev = curr;
+      curr = curr.right;
+    }
+  }
+
+  if (first && second) {
+    const temp = first.val;
+    first.val = second.val;
+    second.val = temp;
+  }
+}
