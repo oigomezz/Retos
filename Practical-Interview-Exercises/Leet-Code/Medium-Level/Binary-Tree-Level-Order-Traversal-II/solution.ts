1e065de223ae15d7f207fa250aa5cb9cc7b64525
oigomezz <oigomezz@unal.edu.co>
2025-07-18 15:45:19 -0500

    Binary Tree Level Order Traversal II

---
+++ Practical-Interview-Exercises/Leet-Code/Medium-Level/Binary-Tree-Level-Order-Traversal-II/solution.ts
@@ -0,0 +1,24 @@
+import { TreeNode } from "../../../tree-node";
+
+function levelOrderBottom(root: TreeNode | null): number[][] {
+  if (!root) return [];
+
+  const result: number[][] = [];
+  const queue: TreeNode[] = [root];
+
+  while (queue.length > 0) {
+    const levelSize = queue.length;
+    const level: number[] = [];
+
+    for (let i = 0; i < levelSize; i++) {
+      const node = queue.shift()!;
+      level.push(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+
+    result.push(level);
+  }
+
+  return result.reverse();
+}
