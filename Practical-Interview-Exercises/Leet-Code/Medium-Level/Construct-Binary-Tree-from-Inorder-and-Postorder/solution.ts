6864a1f2b239ba71ce3b49bfe74b606c9695212e
oigomezz <oigomezz@unal.edu.co>
2025-07-18 15:40:47 -0500

    ✨ Construct Binary Tree from Inorder and Postorder Traversal

---
+++ Practical-Interview-Exercises/Leet-Code/Medium-Level/Construct-Binary-Tree-from-Inorder-and-Postorder/solution.ts
@@ -0,0 +1,18 @@
+import { TreeNode } from "../../../tree-node";
+
+function buildTree(inorder: number[], postorder: number[]): TreeNode | null {
+  const inorderValToIdxMap: Record<number, number> = {};
+  inorder.forEach((v, i) => (inorderValToIdxMap[v] = i));
+  let postOrderRootIdx = postorder.length - 1; //Root idx in postOrder Arr
+  //Start end of Inorder traversa (Inclusive),for splitting the arr without changing
+  function build(start: number, end: number): TreeNode | null {
+    if (start > end) return null;
+    const val = postorder[postOrderRootIdx--];
+    const InorderRootIdx = inorderValToIdxMap[val];
+    const node = new TreeNode(val);
+    node.right = build(InorderRootIdx + 1, end);
+    node.left = build(start, InorderRootIdx - 1);
+    return node;
+  }
+  return build(0, postorder.length - 1);
+}
