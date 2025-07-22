3467ac3bdd71c6deae26a88d87d331156c91eaec
oigomezz <oigomezz@unal.edu.co>
2025-07-22 14:37:35 -0500

    ✨ Path Sum II

---
+++ Practical-Interview-Exercises/Leet-Code/Medium-Level/Path-Sum-II/solution.ts
@@ -0,0 +1,21 @@
+import { TreeNode } from "../../../tree-node";
+
+function pathSum(root: TreeNode | null, targetSum: number): number[][] {
+  const res: number[][] = [];
+
+  const dfs = (node: TreeNode | null, curSum: number, path: number[]): void => {
+    if (!node) return;
+
+    curSum += node.val;
+    path.push(node.val);
+
+    if (!node.left && !node.right && curSum === targetSum) res.push([...path]);
+
+    dfs(node.left, curSum, path);
+    dfs(node.right, curSum, path);
+    path.pop(); // backtrack
+  };
+
+  dfs(root, 0, []);
+  return res;
+}
